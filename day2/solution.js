2e45b60e81571c93a81782ae37443f06d82ccf42
Bernardo Veiga <83598646+JoaoBernardoVeiga@users.noreply.github.com>
2023-02-10 15:39:46 +0000

    exercisesDay2

--- day2/solution.js
+++ day2/solution.js
@@ -1,11 +1,38 @@
 // ---------------------------------------------------------------------------
 function solution1() {
+    let aNumber = 0;
+    let arrayBinary = [];
+    let numberBytes = 0;
+
+    aNumber = Number(prompt("Insira um número decimal para calcular o numero de bits necessarios para representação"));
+    if (aNumber == 0) {
+        numberBytes = 1;
+    } else {
+        while (aNumber != 0) {
+            arrayBinary.unshift(aNumber % 2);
+            aNumber = Math.floor(aNumber / 2);
+        }
+        numberBytes = arrayBinary.length;
+        window.prompt(`O número de bytes é ${numberBytes}`);
+        }
 
 }
 
 
 // ---------------------------------------------------------------------------
 function solution2() {
+    let aNumber = 0;
+    let numberBytes = 0;
+
+    aNumber = Number(prompt("Insira um número decimal para calcular o numero de 1s necessarios para representação binária"));
+    
+        while (aNumber != 0) {
+            if(aNumber%2 == 1){
+            	numberBytes += 1;
+            }
+            aNumber = Math.floor(aNumber / 2);
+        }
+        window.prompt(`O número de 1s é ${numberBytes}`);
 
 }
 
