c8df4e18d4675272741c96c85be1f7446fd4a707
Bernardo Veiga <83598646+JoaoBernardoVeiga@users.noreply.github.com>
2023-03-12 17:57:33 +0000

    started day6 exercise

--- day6/solution.js
+++ day6/solution.js
@@ -1,6 +1,26 @@
 // ---------------------------------------------------------------------------
-function solution1(stringA, stringB) {
 
+let stringA = window.prompt("Insira a primeira string a comparar");
+let stringB = window.prompt("Insira a segunda string a comparar");
+
+//let stringC = window.prompt("Insira uma string a inverter");
+
+function solution1(stringA, stringB) {
+    let counter = 0;
+    
+    AIsLonger = (stringA > stringB);
+    if(AIsLonger){
+    	console.log(AIsLonger);
+    }
+
+
+    
+
+
+
+
+
+    //window.confirm(`${String(stringALength)}`);
 }
 
 
@@ -15,5 +35,5 @@ function solution2(stringC) {
 // ---------------------------------------------------------------------------
 // ---------------------------------------------------------------------------
 // Execute exercise functions
-solution1()
-solution2()
+solution1(stringA, stringB)
+//solution2(stringC)
