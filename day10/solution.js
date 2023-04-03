d4b8d469f12da5f314998d458e442370b77c98d1
Bernardo Veiga <83598646+JoaoBernardoVeiga@users.noreply.github.com>
2023-04-03 12:07:55 +0100

    day10

--- day10/solution.js
+++ day10/solution.js
@@ -1,12 +1,33 @@
+let stringA = window.prompt(`Insira a frase para contar o número de palavras.`);
+let stringB = window.prompt(`Insira a palavra a verificar se é paléndrome.`)
 // ---------------------------------------------------------------------------
 function solution1(stringA) {
-
+	let charInString = "";
+  let wordCount = 1;
+    
+  for(let index = 0; index < stringA.length; index++ ){  	
+  	if(stringA[index] === " "){
+    	wordCount++;
+    }
+  }
+  window.confirm(`A frase "${stringA}" tem ${wordCount} palavras`);
 }
 
 
 // ---------------------------------------------------------------------------
 function solution2(stringB) {
-
+	let reversedWord = "";
+  
+  for(let index = stringB.length - 1; index >= 0; index--){
+  	reversedWord += stringB[index];
+  }
+  
+  console.log(reversedWord)
+  if(stringB.toLowerCase() === reversedWord.toLowerCase()){
+  	window.confirm(`A palavra "${stringB}" é um paléndrome`);
+  } else {
+  	window.confirm(`A palavra "${stringB}" não é um paléndrome`);
+  }
 }
 
 
@@ -15,5 +36,5 @@ function solution2(stringB) {
 // ---------------------------------------------------------------------------
 // ---------------------------------------------------------------------------
 // Execute exercise functions
-solution1()
-solution2()
+solution1(stringA)
+solution2(stringB)
